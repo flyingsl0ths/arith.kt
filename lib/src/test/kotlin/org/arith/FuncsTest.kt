5913bd7d800d717a0be8274de060b15cb5b6decb
flyingsl0ths <randomemail33110@protonmail.com>
2023-09-09 14:32:09 -0700

    Make tests into freehand functions

--- lib/src/test/kotlin/org/arith/FuncsTest.kt
+++ lib/src/test/kotlin/org/arith/FuncsTest.kt
@@ -1,28 +1,24 @@
 package org.arith
 
+import kotlin.test.assertEquals
 import org.arith.utils.atLeastNValues
 import org.arith.utils.guard
 import org.arith.utils.left
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
-class FuncsTest private constructor() {
-    @Test
-    fun givenLessThanNValuesReturnsFalse() {
-        assertEquals(false, atLeastNValues<Int>(10)(listOf(1)))
-    }
+@Test
+fun givenLessThanNValuesReturnsFalse() {
+    assertEquals(false, atLeastNValues<Int>(10)(listOf(1)))
+}
 
-    @Test
-    fun givenAFalseValueReturnsLeft() {
-        val actual =
+@Test
+fun givenAFalseValueReturnsLeft() {
+    val actual =
             guard<Int>(
-                "givenAFalseValueReturnsLeft" to "errors on predicate",
-                { it.first() == 11 },
-                { it.size })(listOf(10))
+                    "givenAFalseValueReturnsLeft" to "errors on predicate",
+                    { it.first() == 11 },
+                    { it.size }
+            )(listOf(10))
 
-        assertEquals(
-            left("givenAFalseValueReturnsLeft: errors on predicate"),
-            actual
-        )
-    }
+    assertEquals(left("givenAFalseValueReturnsLeft: errors on predicate"), actual)
 }
